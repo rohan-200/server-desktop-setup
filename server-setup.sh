2e43da18f199b5438dcde13e7e5f00b12c26a812
rohan-200 <rohan18mondal@gmail.com>
2022-05-27 18:51:15 +0000

    added ssh

--- server-setup.sh
+++ server-setup.sh
@@ -1,5 +1,6 @@
-# sudo sh update.sh
-# sudo sh java-install.sh
-# sudo sh mvn-install.sh
-# sudo sh configure-java.sh
+sudo sh ssh-setup.sh
+sudo sh update.sh
+sudo sh java-install.sh
+sudo sh mvn-install.sh
+sudo sh configure-java.sh
 sudo sh mongodb.sh
