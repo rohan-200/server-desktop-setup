236595f91f9f961a4858d7acf883bddf0171f545
rohan-200 <rohan18mondal@gmail.com>
2022-05-28 07:58:28 +0530

    testing

--- ssh-setup/ssh-setup.sh
+++ ssh-setup/ssh-setup.sh
@@ -1,5 +1,8 @@
-sudo apt install openssh-server -y
+sudo apt-get install openssh-server -y
+sudo systemctl enable ssh
+
 sudo ufw enable
+sudo ufw allow ssh
 sudo ufw allow 22/tcp
 
 echo "done setting ssh server can be used headless "
