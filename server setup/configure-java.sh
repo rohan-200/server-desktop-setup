ef5e33789f2aade4a34d941d76ed6a849228d1d1
rohan-200 <rohan18mondal@gmail.com>
2022-05-28 08:07:31 +0530

    testing

--- server setup/configure-java.sh
+++
@@ -1,8 +0,0 @@
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-12.0.2/bin/java 1
-if [ $? -eq 0 ]; then
-   sudo update-alternatives --config java <<< 2
-   sleep 2
-else
-   echo ${Red}FAIL
-   sleep 2
-fi
--- server setup/server-setup.sh
+++
@@ -1,6 +0,0 @@
-sudo sh ssh-setup.sh
-sudo sh update.sh
-sudo sh java-install.sh
-sudo sh mvn-install.sh
-sudo sh configure-java.sh
-sudo sh mongodb.sh
